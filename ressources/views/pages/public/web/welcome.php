5fd2ec299fc1f82653a2ffcfd9b6e4f0280de672
gedeontimothy <gedeonbateko3@gmail.com>
2022-10-03 15:08:13 +0100

    Add | Create | Update : Deleting useless tags and code reduction on the view and CSS code separation

--- ressources/views/pages/public/web/welcome.php
+++ ressources/views/pages/public/web/welcome.php
@@ -10,52 +10,29 @@
 			<title><?= response('public:web')['welcome']['title']; ?></title>
 
 			<!-- ICON -->
-			<link href="<?= route('external-url', ['url' => cdn('icon/svg-modify/icons8_send_to_back.svg')]); ?>" rel="icon" type="image/svg">
+			<link href="<?php $a = route('external-url', ['url' => cdn('icon/svg-modify/icons8_send_to_back.svg')]); echo $a ? $a : cdn('icon/svg-modify/icons8_send_to_back.svg') ?>" rel="icon" type="image/svg">
 
 			<!-- STYLESHEET -->
-			<link rel="stylesheet" type="text/css" href="<?= route('external-url', ['url' => cdn('css/font.min.css')]); ?>">
-			<link rel="stylesheet" type="text/css" href="<?= route('external-url', ['url' => cdn('css/keyframe.min.css')]); ?>">
-			<link rel="stylesheet" type="text/css" href="<?= route('external-url', ['url' => cdn('css/f-family.css')]); ?>">
-			<link rel="stylesheet" type="text/css" href="<?= route('external-url', ['url' => cdn('css/tailwind.min-2.2.2.css')]); ?>">
-			<style type="text/css">
-				.z-1-{z-index: -1;}
-				.back-image-dheje{filter: blur(30px);}
-				@keyframes kkio {0%{filter: blur(0px);}100%{filter: blur(30px);}}
-				.back-color-dheje{background-color: white;opacity: .6;animation: opacity-10 1.3s, opacity-10-6 4s 1.3s;}
-				@keyframes jdwi {0%{opacity: .4;}100%{opacity: .6;}}
-				.back{z-index: -1;}
-
-				/* --- Color ---  */
-				.zo-sjbwdw{background-image: linear-gradient(35deg, rgba(255, 255, 255, .4) 30%, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0));}
-				.parent-bdwjdw{animation: opacity-0 4.3s, opacity-0-10 4.5s 4s ease-in, translate-x-30 4.5s 4s ease-in reverse;}
-				@media (prefers-color-scheme: dark) {
-					*{color: white;}
-					html{background: rgb(10,10,10);}
-					.back-image-dheje{filter: blur(20px);}
-					@keyframes kkio {0%{filter: blur(0px);}100%{filter: blur(20px);}}
-					.back-color-dheje{background-color: black;opacity: .9;animation: opacity-10 1.3s, opacity-10-9 4s 1.3s;}
-					@keyframes jdwi {0%{opacity: .4;}100%{opacity: .9;}}
-					/*.zo-sjbwdw{background-color: rgba(0, 0, 0, .6);}*/
-					.zo-sjbwdw{background-image: linear-gradient(45deg, rgba(0, 0, 0, .6) 45%, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0));}
-				}
-			</style>
-			<script type="text/javascript" src="<?= route('external-url', ['url' => cdn('js/jquery-3.6.0.js')]); ?>"></script>
+			<link rel="stylesheet" type="text/css" href="<?= assets('css/excepts/public/web/welcome.css'); ?>">
+			<link rel="stylesheet" type="text/css" href="<?php $a = route('external-url', ['url' => cdn('css/font.min.css')]); echo $a ? $a : cdn('css/font.min.css'); ?>">
 		</head>
-		<body class="z-10 w-full h-full">
-			<div class="back fixed left-0 top-0 w-full h-full m-auto overflow-hidden">
-				<div class="back-image-dheje h-full w-full absolute bg-no-repeat bg-center bg-cover" style="background-image: url(<?= route('external-url', ['url' => cdn('image/10/' . random_int(0, 32))]); ?>);"></div>
-				<div class="back-color-dheje h-full w-full absolute z-10"></div>
+		<body class="w-full-29u2 h-full-ej2 overflow-hidden-w19" style="z-index: 10;">
+			<div class="back w-full-29u2 h-full-ej2 overflow-hidden-w19" style="position: fixed;">
+				<div class="back-image-dheje h-full-ej2 w-full-29u2 absolute-329u bg-dyh82" style="background-image: url(<?= assets('img/ash-edmonds-0aWZdK8nK2I-unsplash.jpg'); ?>);"></div>
+				<div class="back-color-dheje h-full-ej2 w-full-29u2 absolute-329u"></div>
 			</div>
-			<main class="h-screen w-screen flex justify-center items-center">
-				<div class="flex w-1/2">
-					<div class="parent-bdwjdw h-full w-full overflow-hidden rounded-3xl relative">
-						<div class="zo-sjbwdw absolute w-full h-full z-1-"></div>
-						<div class="flex-col p-10 space-y-9">
-							<div class="ff-Nunito-Bold text-5xl"><?= response('public:web')['welcome']['page_message']; ?></div>
-							<a href="<?= route('home') ? route('home') : route('chat'); ?>" class="inline-block ml-8">
-								<div class="flex items-center space-x-2">
-									<span class="ff-Nunito-Light text-sm text-left"><?= config("name"); ?></span>
-									<span class="w-5 h-5 inline-block"><?php include cdn('icon/svg/icons8_right_arrow.svg'); ?></span>
+			<main class="main-w2181 flex-eu29 items-center-ewu29">
+				<div class="flex-eu29">
+					<div class="parent-bdwjdw h-full-ej2 w-full-29u2 overflow-hidden-w19" style="border-radius: 1.5rem;position: relative;">
+						<div class="zo-sjbwdw absolute-329u w-full-29u2 h-full-ej2 z-1-"></div>
+						<div class="fleco-2u92">
+							<div class="page-title-eu92"><?= response('public:web')['welcome']['page_title']; ?></div>
+							<a href="<?= url(''); ?>" class="link-w29">
+								<div class="flex-eu29 items-center-ewu29">
+									<span style="font-family: 'Nunito Light', 'Segoe Semilight', 'Segoe UI Semilight', 'Cocogoose UltraLight', arial, sans-serif;text-align: left; font-size: 0.875rem; line-height: 1.25rem;"><?= config("name"); ?></span>
+									<span style="width: 1.25rem; height: 1.25rem; display: inline-block;">
+										<svg style="fill:currentColor;" class="svg-right_arrow" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32"><path d="M21.1875 9.28125L19.78125 10.71875L24.0625 15L4 15L4 17L24.0625 17L19.78125 21.28125L21.1875 22.71875L27.90625 16Z"></path></svg>
+									</span>
 								</div>
 							</a>
 						</div>
